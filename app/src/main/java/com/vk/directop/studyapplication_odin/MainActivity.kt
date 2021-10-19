240c07d9b494027ba34015cff29b3095296c9903
PavelWrki5W10 <fire_ra@bk.ru>
2021-10-19 16:25:45 +0300

    create project - first commit

---
+++ app/src/main/java/com/vk/directop/studyapplication_odin/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.vk.directop.studyapplication_odin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+// инициализировать
+// git init
+// добавить все файлы в репозиторий
+// git add .
+// git commit -m "create project - this is comment"
+// git remote add origin https://github.com/fffgf
